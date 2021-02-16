e92f74f93cd06af8b7129310a0b388bb57f1ab17
erinmayg <erinmaygunawan@gmail.com>
2021-02-17 06:22:52 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [query, setQuery] = useState('mountains');
+  const [pictures, setPictures] = useState([]);
+
+  useEffect(() => {
+    const apiRoot = 'https://api.unsplash.com';
+    const accessKey = process.env.ACCESS_KEY;
+    axios
+      .get(
+        `${apiRoot}/search/photos?query=${query}&client_id=${accessKey}&count=10`
+      )
+      .then((res) => setPictures(res.data.results));
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <div className='button-row'>
+        <div
+          className='button'
+          onClick={(e) => {
+            setQuery('mountains');
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Mountains
+        </div>
+        <div
+          className='button'
+          onClick={(e) => {
+            setQuery('beaches');
+          }}
+        >
+          Beaches
+        </div>
+        <div
+          className='button'
+          onClick={(e) => {
+            setQuery('birds');
+          }}
+        >
+          Birds
+        </div>
+        <div
+          className='button'
+          onClick={(e) => {
+            setQuery('food');
+          }}
+        >
+          Food
+        </div>
+      </div>
+      <h1>{query.charAt(0).toUpperCase() + query.substring(1)} Pictures</h1>
+      <div className='pictures-container'>
+        {pictures?.map((pic) => (
+          <div className='card' key={pic.id}>
+            <img
+              className='card--image'
+              alt={pic.alt_description}
+              src={pic.urls.full}
+              width='50%'
+              height='50%'
+            ></img>
+          </div>
+        ))}
+        ;
+      </div>
     </div>
   );
 }
